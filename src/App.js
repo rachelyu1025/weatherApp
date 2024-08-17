afb7b1109e6eb5bc24461561cdb7f2a0f2415e0a
EUNBI <rachelyu.1025@gmail.com>
2024-08-17 16:45:38 +0900

    env 파일 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return <div></div>;
+  const initLocation = {
+    latitude: 0,
+    longitude: 0,
+  };
+
+  const [currentLocation, setCurrentLocation] = useState(initLocation);
+
+  const getCurrLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setCurrentLocation((prev) => ({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      }));
+    });
+  };
+
+  useEffect(() => {
+    getCurrLocation();
+  }, []);
+
+  return (
+    <div>
+      <div>
+        current: {(currentLocation.latitude, currentLocation.longitude)}
+      </div>
+    </div>
+  );
 }
 
 export default App;
