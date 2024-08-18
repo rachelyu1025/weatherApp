4f1222a07ab9ad40f3fb73c2cd864d6059a6f818
EUNBI <rachelyu.1025@gmail.com>
2024-08-18 18:12:13 +0900

    ✨ [feat] react-spinners 적용 및 에러처리

--- src/App.js
+++ src/App.js
@@ -5,15 +5,30 @@ import Select from './components/Select';
 import WeatherImage from './components/WeatherImage';
 import DateBox from './components/DateBox';
 import WeatherInfo from './components/WeatherInfo';
+import { FadeLoader } from 'react-spinners';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
   const [weather, setWeather] = useState();
   const [city, setCity] = useState(null);
 
   // 현재 위치정보에 따른 날씨 정보 호출 함수
   const getWeatherByCurrLocation = async (lat, lon) => {
+    setIsLoading((prev) => true);
+
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`;
-    const response = await axios.get(url).then((res) => res.data);
+    const response = await axios
+      .get(url)
+      .then((res) => {
+        setIsLoading((prev) => false);
+        return res.data;
+      })
+      .catch((error) => {
+        setIsLoading((prev) => false);
+        setIsError((prev) => true);
+      });
 
     setWeather((prev) => response);
   };
@@ -38,16 +53,32 @@ function App() {
   }, [getCurrLocation, city]);
 
   return (
-    <div className='container'>
-      <div className='header'>
-        <DateBox />
+    <div>
+      <FadeLoader loading={isLoading} color={`skyblue`} speedMultiplier={1.5} />
 
-        <Select city={city} setCity={setCity} current={weather?.name} />
-      </div>
+      {isError ? (
+        <div>
+          <img
+            className='error'
+            src='./assets/404_error.png'
+            alt='error page'
+          />
+        </div>
+      ) : (
+        !isLoading && (
+          <div className='container'>
+            <div className='header'>
+              <DateBox />
 
-      <WeatherImage mainState={weather?.weather[0].main} />
+              <Select city={city} setCity={setCity} current={weather?.name} />
+            </div>
 
-      <WeatherInfo weather={weather} />
+            <WeatherImage mainState={weather?.weather[0].main} />
+
+            <WeatherInfo weather={weather} />
+          </div>
+        )
+      )}
     </div>
   );
 }
